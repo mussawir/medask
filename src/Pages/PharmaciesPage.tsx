53b69c0639ee1c4a05eddb623606fa389834cb85
Ibrahim brohi <133601225+Meer786777@users.noreply.github.com>
2024-10-02 05:52:35 -0700

    prago change

--- src/Pages/PharmaciesPage.tsx
+++ src/Pages/PharmaciesPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Form from '../components/Form'
 import Pharpage1 from '../components/Pharmaciespage'
-import Pharmacies from '../components/Pharmacies'
+import Phsection2 from '../components/Phsection2'
 import Phsection3 from '../components/Phsection3'
 import Phsection4 from '../components/Phsection4'
 import Phsection5 from '../components/Phsection5'
@@ -12,6 +12,7 @@ function PharmaciesPage() {
   return (
 <>
         <Pharpage1/>
+        <Phsection2/>
         <Phsection3/>
         <Phsection4/>
         <Phsection5/>
