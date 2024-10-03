4c6ba501276ff8126c4bb11cfdb614eeed3812de
Ibrahim brohi <133601225+Meer786777@users.noreply.github.com>
2024-10-03 02:06:55 -0700

    phsection 2 3 changes

--- src/components/Phsection3.tsx
+++ src/components/Phsection3.tsx
@@ -5,18 +5,18 @@ import phsection3Image2 from '../images/phsection3Image2.png';
 
 const Phsection3: React.FC = () => {
   return (
-    <div className="Phsection3">
+    <div className="prag-Phsection3">
       {/* Image Section */}
-      <div className="image-section3">
-      <img src={phsec3mainImage} alt="Healthcare professionals" className="image-item13" />
-      <img src={phsection3Image2} alt="Discussion in healthcare" className="image-item23" />
+      <div className="prag-image-section3">
+      <img src={phsec3mainImage} alt="Healthcare professionals" className="prag-image-item13" />
+      <img src={phsection3Image2} alt="Discussion in healthcare" className="prag-image-item23" />
       </div>
 
       {/* Text Section */}
-      <div className="text-section3">
-        <h1 className="brand-name3">Medask</h1>
-        <h2 className="section-title3">Retail Pharmacies</h2>
-        <p className="section-description3">
+      <div className="prag-text-section3">
+        <h1 className="prag-brand-name3">Medask</h1>
+        <h2 className="prag-section-title3">Retail Pharmacies</h2>
+        <p className="prag-section-description3">
           There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
           in some form, by injected humour, or randomised words which don't look even slightly believable.
           If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden
