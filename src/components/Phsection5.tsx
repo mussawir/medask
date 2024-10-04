9b387ce8f647761815602a13c205357261b30a57
mussawir20 <mussawir20@gmaill.com>
2024-10-04 20:14:25 +0500

    pharmacy page responsive and other updates

--- src/components/Phsection5.tsx
+++ src/components/Phsection5.tsx
@@ -7,6 +7,8 @@ import phsection5Image3 from '../images/phsection53.png';
 const Phsection5: React.FC = () => {
   return (
     <div className="phsection5">
+              <h1 className="brand-name5 ibrtitle1">Medask</h1>
+              <h2 className="section-title5">Commercial-Flagship Pharmacies</h2>
       {/* Image Section */}
       <div className="image-section5">
         <div className="image-item-wrap">
@@ -22,8 +24,7 @@ const Phsection5: React.FC = () => {
 
       {/* Text Section */}
       <div className="text-section5">
-        <h1 className="brand-name5 ibrtitle1">Medask</h1>
-        <h2 className="section-title5">Commercial-Flagship Pharmacies</h2>
+
         <p className="section-description5">
           There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
         </p>
