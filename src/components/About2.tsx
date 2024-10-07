c63740872b8252eb79172f026ca4d2db86612350
Ibrahim brohi <133601225+Meer786777@users.noreply.github.com>
2024-10-07 00:41:26 -0700

    about2

--- src/components/About2.tsx
+++ src/components/About2.tsx
@@ -1,6 +1,7 @@
 import '../App.css';
 import React from 'react';
 import Icon from '../images/map-solid'
+import Icon2 from '../images/plan2'
 function About2() {
     return (
         <div className="about2bg">
@@ -9,33 +10,33 @@ function About2() {
                 <h1 className='about2h2 subtitle'>Our Services</h1>
             </div>
             <div className="about2cen">
-            <div className="aboutdiv1">
-                <div className="aboutdiv2">
-                    <Icon />
-                    <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
-                    <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
-                    <div className='arrw'></div>
-                </div>
-                <div className="aboutdiv2">
-                    <Icon />
-                    <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
-                    <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
-                    <div className='arrw'></div>
-                </div>
-                 <div className="aboutdiv2">
-                    <Icon />
-                    <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
-                    <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
-                    <div className='arrw'></div>
-                </div>
-                <div className="aboutdiv2">
-                    <Icon />
-                    <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
-                    <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
-                    <div className='arrw'></div>
+                <div className="aboutdiv1">
+                    <div className="aboutdiv2">
+                        <Icon />
+                        <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
+                        <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        <div className='arrw'></div>
+                    </div>
+                    <div className="aboutdiv2">
+                        <Icon2/>
+                        <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
+                        <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        <div className='arrw'></div>
+                    </div>
+                    <div className="aboutdiv2">
+                        <Icon />
+                        <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
+                        <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        <div className='arrw'></div>
+                    </div>
+                    <div className="aboutdiv2">
+                        <Icon />
+                        <h1 className='about2h3'>Lorem Ipsum Dolor</h1>
+                        <p className='aboutp1'>It is a long established fact that a reader will be distracted by the readable content.</p>
+                        <div className='arrw'></div>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 
